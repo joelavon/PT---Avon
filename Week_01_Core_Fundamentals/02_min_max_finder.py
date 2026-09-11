9fa9e7155612936333d33474c2d270061465fd23
Joelavon <45958@rws.uk.net>
2026-09-11 13:43:42 +0100

    min max done

--- Week_01_Core_Fundamentals/02_min_max_finder.py
+++ Week_01_Core_Fundamentals/02_min_max_finder.py
@@ -12,12 +12,20 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
-
-
-if __name__ == "__main__":
-    main()
+numbers = []
+x = int(input("How many numbers would you like to enter?"))
+for i in range(x):
+  a = int(input("Enter a number: "))
+  numbers.append(a)
+def find_min_max(numbers):
+  length = len(numbers)
+  mx = numbers[0]
+  for i in range(length):
+    if numbers[i] > mx:
+      mx = numbers[i]
+  mn = numbers[0]
+  for i in range(length):
+    if numbers[i] < mn:
+      mn = numbers[i]
+  return (mn, mx)
+print(find_min_max(numbers))
