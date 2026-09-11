98ec2f7b7a682ca1ae421ea9e6387ecdcb6a2383
Joelavon <45958@rws.uk.net>
2026-09-11 13:18:44 +0100

    completed

--- Week_01_Core_Fundamentals/01_average_calculator.py
+++ Week_01_Core_Fundamentals/01_average_calculator.py
@@ -14,10 +14,23 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
+import numbers
+
+
 def main():
-    pass
-
-
+  values = []
+  a = int(input("How many numbers would you like to enter?"))
+  while a<1:
+    a = int(input("How many numbers would you like to enter?"))
+  for i in range(a):
+    b = int(input("Enter a number"))
+    values.append(b)
+  def calculate_average(values):
+    average = sum(values) / len(values)
+    return average
+  average = calculate_average(values)
+  print("The average is", average)
+  pass
 
 if __name__ == "__main__":
     main()
