67990ec1229f880b9c2ab2baf7290293c5966e18
Joelavon <45958@rws.uk.net>
2026-09-12 08:45:35 +0100

    done

--- Week_01_Core_Fundamentals/03_array_reversal.py
+++ Week_01_Core_Fundamentals/03_array_reversal.py
@@ -12,12 +12,15 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
-
-
-if __name__ == "__main__":
-    main()
+import random
+list = []
+for i in range(10):
+    a = random.randint(1, 10)
+    list.append(a)
+def reverse_list(list):
+  reversed_list = []
+  for i in range(10):
+    reversed_list.append(list[9-i])
+  return reversed_list
+print(list)
+print(reverse_list(list))
--- Week_01_Core_Fundamentals/04_linear_search.py
+++ Week_01_Core_Fundamentals/04_linear_search.py
@@ -13,12 +13,16 @@ TODO:
 - Add demonstration code under `if __name__ == "__main__":`
 """
 
-def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
-    pass
-
-
-if __name__ == "__main__":
-    main()
+import random
+target = int(input("Enter a target value you would like to search for"))
+list = []
+for i in range(10):
+    a = random.randint(1, 10)
+    list.append(a)
+def linear_search(list, target):
+    for i in range(10):
+      if list[i] == target:
+        return i
+    return -1
+print(linear_search(list,target))
+print(list)
